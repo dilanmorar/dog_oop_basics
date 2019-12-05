60eabd3233e25ec7a51b66d9240c12a65a7c76f6
Dilan Morar <DMorar@spartaglobal.com>
2019-12-05 20:14:02 +0000

    dog class has a default value and works

--- Dog_class_tests.py
+++ Dog_class_tests.py
@@ -8,8 +8,9 @@ print(dog1.greet_owner())
 
 print(dog1.name)
 print(dog1.owner)
+print(dog1.dog_collar)
 
-dog2 = Dog('David', 'Herbie')
+dog2 = Dog('David', 'Herbie', 'red')
 
 print(dog2.eat_bone())
 print(dog2.run())
@@ -17,3 +18,4 @@ print(dog2.greet_owner())
 
 print(dog2.name)
 print(dog2.owner)
+print(dog2.dog_collar)
