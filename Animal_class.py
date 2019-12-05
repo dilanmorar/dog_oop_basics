0106d68378a4b2ca3dba8ef0aca2f7a46f676d00
Dilan Morar <DMorar@spartaglobal.com>
2019-12-05 16:38:05 +0000

    animal class created

---
+++ Animal_class.py
@@ -0,0 +1,15 @@
+# create animal class
+
+class Animal():
+    # define characteristics
+    def __init__(self):
+        self.alive = True
+        self.bones = True
+    # define methods
+    def eat(self):
+        return
+    def sleep(self):
+        return
+    def speak(self):
+        return
+    
